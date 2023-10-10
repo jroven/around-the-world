a1e897f7cd216ca9516f98b0b2404b8dddfc52dd
jroven <jonathanroven@gmail.com>
2023-10-10 18:22:43 -0500

    finished week 2 project

--- src/Player.hs
+++ src/Player.hs
@@ -3,6 +3,8 @@ module Player where
 import Item
 import Room
 
+import Data.List
+
 data Player
     = Player { inventory :: [ItemName]
              , maxWeight :: Integer
@@ -10,4 +12,15 @@ data Player
     deriving (Show, Eq)
 
 addItem :: ItemName -> Player -> Player
-addItem i p = undefined
+addItem i p = Player (i : (inventory p)) (maxWeight p) (location p)
+
+removeItem :: ItemName -> Player -> Player
+removeItem i p = Player (Data.List.delete i (inventory p))
+                        (maxWeight p)
+                        (location p)
+
+newLocation :: RoomName -> Player -> Player
+newLocation r p = Player (inventory p) (maxWeight p) r
+
+isCarryingAnything :: Player -> Bool
+isCarryingAnything p = inventory p /= []
